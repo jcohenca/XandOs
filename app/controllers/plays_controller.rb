95974b89eb1c58f03fee13986af1560ff6291d1a
jcohenca <jcohenca@gmail.com>
2014-08-12 16:46:28 -0400

    graf

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -2,7 +2,7 @@ class PlaysController < ApplicationController
 
   def index
      
-    @search = Play.where("city = ? AND period = ? AND year = ? AND month = ? AND date = ? AND game_clock BETWEEN ? - 1.00 AND ? + 1.00", params[:city], params[:period], params[:year], params[:month], params[:date], params[:gc], params[:gc]).order(game_clock: :asc).limit(1000)
+    @search = Play.where("year = ? AND month = ? AND date = ? AND city = ? AND period = ? AND game_clock BETWEEN ? - 1.00 AND ? + 1.00", params[:year], params[:month], params[:date], params[:city], params[:period], params[:gc], params[:gc]).order(game_clock: :asc).limit(1000)
 
     # @search = Play.where("city = ? AND period = ?", params[:city], params[:period]).order(shot_clock: :asc).limit(100)
 
