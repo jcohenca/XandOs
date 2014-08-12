7165e258a1940ed836f57101a8bf393c03005ca5
jcohenca <jcohenca@gmail.com>
2014-08-12 00:08:37 -0400

    psql

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -6,7 +6,7 @@ class PlaysController < ApplicationController
     # @search = Play.where("city = ? AND period = ? AND year = ? AND month = ? AND date = ? AND game_clock BETWEEN ? - 1.00 AND ? + 1.00", params[:city], params[:period], params[:year], params[:month], params[:date], params[:gc], params[:gc]).order(game_clock: :asc).limit(1000)
 
 
- @search = Play.where("city = ? AND period = ?", params[:city], params[:period]).order(shot_clock: :asc).limit(1000)
+ @search = Play.where("city = ? AND period = ?", params[:city], params[:period]).order(shot_clock: :asc).limit(100)
 
 
     if @search.length > 10
