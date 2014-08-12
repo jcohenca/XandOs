8f2a4cd09b647b4eba57cf34f094e023ac6a7c55
jcohenca <jcohenca@gmail.com>
2014-08-11 20:43:22 -0400

    fix

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -3,7 +3,11 @@ class PlaysController < ApplicationController
   def index
      
 
-    @search = Play.where("city = ? AND period = ? AND year = ? AND month = ? AND date = ? AND game_clock BETWEEN ? - 1.00 AND ? + 1.00", params[:city], params[:period], params[:year], params[:month], params[:date], params[:gc], params[:gc]).order(game_clock: :asc).limit(1000)
+    # @search = Play.where("city = ? AND period = ? AND year = ? AND month = ? AND date = ? AND game_clock BETWEEN ? - 1.00 AND ? + 1.00", params[:city], params[:period], params[:year], params[:month], params[:date], params[:gc], params[:gc]).order(game_clock: :asc).limit(1000)
+
+
+ @search = Play.where("city = ? AND period = ? AND year = ? AND month = ? AND date = ?", params[:city], params[:period], params[:year], params[:month], params[:date]).order(shot_clock: :asc).limit(1000)
+
 
     if @search.length > 10
       @finalsearch = Play.where(:posession_id => @search.first.posession_id)
