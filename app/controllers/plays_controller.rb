45790435aa47b457f30397e4f32c55456d779ce1
jcohenca <jcohenca@gmail.com>
2014-08-11 23:23:31 -0400

    psql

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -15,7 +15,7 @@ class PlaysController < ApplicationController
 
 
     # if @search.length < 1
-      @search = Play.where("city = ?", params[:city]).limit(100)
+      @search = Play.find_by_sql("city = 'Phoenix').limit(100)
 
       # @search = Play.find_by_sql("SELECT * FROM AnimatedXandOs_development.plays;")
     # end
