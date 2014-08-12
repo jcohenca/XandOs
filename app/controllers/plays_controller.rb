79a27aa43b89184f2417c94b2028fcd57523739b
jcohenca <jcohenca@gmail.com>
2014-08-11 23:58:55 -0400

    psql

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -6,17 +6,17 @@ class PlaysController < ApplicationController
     # @search = Play.where("city = ? AND period = ? AND year = ? AND month = ? AND date = ? AND game_clock BETWEEN ? - 1.00 AND ? + 1.00", params[:city], params[:period], params[:year], params[:month], params[:date], params[:gc], params[:gc]).order(game_clock: :asc).limit(1000)
 
 
- # @search = Play.where("city = ? AND period = ? AND year = ? AND month = ? AND date = ?", params[:city], params[:period], params[:year], params[:month], params[:date]).order(shot_clock: :asc).limit(1000)
+ @search = Play.where("city = ? AND period = ? AND year = ? AND month = ? AND date = ?", params[:city], params[:period], params[:year], params[:month], params[:date]).order(shot_clock: :asc).limit(1000)
 
 
-    # if @search.length > 10
-    #   @finalsearch = Play.where(:posession_id => @search.first.posession_id)
-    # end
+    if @search.length > 10
+      @finalsearch = Play.where(:posession_id => @search.first.posession_id)
+    end
 
 
     # if @search.length < 1
       # @search = Play.where("city = ?",'Phoenix').limit(100)
-      @search = Play.where("city = ?", params[:city]).limit(100)
+      # @search = Play.where("city = ?", params[:city]).limit(100)
 
       # @search = Play.find_by_sql("SELECT * FROM AnimatedXandOs_development.plays;")
     # end
