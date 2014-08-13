cff61931a4f16173564c73c3c59632cf4a9ff1dc
jcohenca <jcohenca@gmail.com>
2014-08-13 09:53:19 -0400

    graf

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -5,8 +5,11 @@ class PlaysController < ApplicationController
     @search = Play.where("year = ? AND month = ? AND date = ? AND city = ? AND period = ? AND game_clock BETWEEN ? - 1.00 AND ? + 1.00", params[:year], params[:month], params[:date], params[:city], params[:period], params[:gc], params[:gc]).order(game_clock: :desc).limit(1000)
 
     if @search.length > 10
-      @finalsearch = Play.where(:possession_id => @search.first.possession_id).order(game_clock: :asc).limit(500)
+      @finalsearch = Play.where(:possession_id => @search.first.possession_id).order(game_clock: :asc)
     end
+
+
+    @possession_id = Play.where("possession_id = ?", params[:pos_id] )
 
     respond_to do |format|
         format.html {}
